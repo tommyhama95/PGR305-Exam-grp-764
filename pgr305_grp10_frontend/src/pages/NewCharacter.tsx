a96eebaf6eadb74343aa59e551866a53ced88877
Krister <SeventhDisaster@outlook.com>
2020-11-24 17:21:18 +0100

    Created routes of new object sites

---
+++ pgr305_grp10_frontend/src/pages/NewCharacter.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewCharacter = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default NewCharacter
---
+++ pgr305_grp10_frontend/src/pages/NewGame.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewGame = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default NewGame
